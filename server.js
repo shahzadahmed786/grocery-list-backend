d415cb043dcf701f960cf9068b9b8991142d8893
shahzadahmed786 <shahzadahmed7396@gmail.com>
2019-04-20 14:27:14 +0530

    added

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const server = express()
+const port = 3003
+
+server.get('/', (req, res) => res.send('Hello World!'))
+
+server.get('/api/itemList', (req, res) => res.send('shaizu!'))
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
