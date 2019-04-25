630ca57a347372de50c87dd415a7eb160949a834
shahzadahmed786 <shahzadahmed7396@gmail.com>
2019-04-25 13:11:36 +0530

    added grocery back-end file

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
 const express = require('express')
 const server = express()
 const port = 3003
-const GroceryItems = [
-    {
-        name : "tamaoes",
-        quantity : 1,
-        amount :  20
-    },
-    {
-        name : "potatoes",
-        quantity : 2,
-        amount :  40
-    },
-    {
-        name : "potatoes",
-        quantity : 1,
-        amount :  50
-    },
-    {
-        name : "Brockle",
-        quantity : 1,
-        amount :  80
-    },
-    {
-        name : "Corn Flackes",
-        quantity : 2,
-        amount :  40
-    }
-]
+const { Client } = require('pg')
+const client = new Client()
 
-server.get('/', (req, res) => res.send('Hello World!'))
+server.get('/api/item-list',async (req,res)=>{
+await client.connect()
 
-server.get('/api/itemList', (req, res) => res.send(GroceryItems))
+const dres = await client.query('select * from items');
+await client.end()
+res.send(dres.rows);
+});
+
+
+server.get('/api/item-list', (req, res) => res.send('GroceryItems'));
 
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
